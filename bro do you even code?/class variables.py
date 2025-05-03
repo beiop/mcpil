7c63433285e25a3ca32af50026e201ce5279a7af
Beiop <rolisocool@gmail.com>
2025-05-02 17:11:18 -0700

    ubuntu mcpil2

---
+++ bro do you even code?/class variables.py
@@ -0,0 +1,9 @@
+class Student:
+    
+    class_year = 2024
+    
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+student1 = Student("Spongebob",30)
+student2 = Student("Patrick",55)
