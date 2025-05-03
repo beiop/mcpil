566aeb9c3257365bdf5e7840e0f35934b3e84efc
Beiop <75444801+beiop@users.noreply.github.com>
2025-05-02 17:00:31 -0700

    add bro

---
+++ bro do you even code?/intro.py
@@ -0,0 +1,12 @@
+class Car:
+    def __init__(self, model, year, for_sale):
+        self.model = model
+        self.color = color
+        self.for_sale = for_sale
+        self.year = year
+    def drive(self):
+        print(f"You drive the {self.model}")
+
+car1 = Car("Stingray",2024,"red",False)
+
+print(car1.model)
